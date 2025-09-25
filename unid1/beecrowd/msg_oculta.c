ba4ac56317058e3d51ccf55725ee2f6f6614d558
Gabriel <gabmineiro123@hotmail.com>
2025-09-25 09:27:10 -0400

    beec

---
+++ unid1/beecrowd/msg_oculta.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 60
+
+
+int main(void){
+  char palavra[TAM];
+  char mensagem[TAM];
+  int index_msg;
+  int achou_letra;
+  int vezes;
+  scanf("%d", &vezes);
+
+  for(int i = 0; i < vezes; i++){
+    fgets(palavra, TAM, stdin);
+    palavra[strcspn(palavra, "\n")] = '\0';
+    
+    index_msg = 0;
+
+    for(int j = 0; j < TAM; j++){
+      if(isalpha(palavra[j])){
+        mensagem[index_msg] = palavra[j];
+      }  
+    }
+  }
+}
+
