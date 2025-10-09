f1ea50a0161d89d20cde7b105cf7c2c533201cf2
Gabriel <gabmineiro123@hotmail.com>
2025-10-09 09:16:28 -0400

    beecrowd e pilhas filas

---
+++ unid1/beecrowd/umdoistres.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1010
+
+typedef struct palavra{
+  char palavra[10];
+  int tam;
+}Palavra;
+
+int verifica_zero(Palavra* palavra);
+int verifica_um(Palavra* palavra);
+int verifica_dois(Palavra* palavra);
+
+
+int main(void){
+  int a;
+  scanf("%d", &a);
+  
+  char garb;
+  scanf("%c", &garb);
+  
+  Palavra *palavra = (Palavra*) malloc(sizeof(Palavra));
+  for(int i = 0; i < a; i++){
+    palavra->tam = 0;
+    for(int j = 0; j < MAX; j++){
+      scanf("%c", palavra->palavra[j]);
+      palavra->tam++;
+    }
+    
+    if(verifica_um(palavra) == 1){
+      printf("1\n");
+    } else if(verifica_dois(palavra) == 1){
+      printf("2\n");
+    } else if(verifica_tres(palavra) == 1){
+      printf("3\n");
+    }
+  } 
+}
+
+int verifica_um(Palavra *palavra){
+  if(palavra->tam == 3){
+    if(palavra->palavra[0] == 'o' || palavra->palavra[1] == 'n'){
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+int verifica_dois(Palavra *palavra){
+  if(palavra->tam == 3){
+    if(palavra->palavra[0] == 't' || palavra->palavra[1] == 'w'){
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+int verifica_tres(Palavra *palavra){
+  if(palavra->tam == 5) return 1;
+
+  return 0;
+}
---
+++ unid1/beecrowd/zero.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 1000010
+
+void converter_int_string(char* string, int num);
+
+int main(void){
+  int a;
+  int b;
+  
+  scanf("%d %d", &a, &b);
+  
+  char resultado[TAM];
+  
+  for(int i = 0; i < )
+  
+}
+
+void converter_int_string(char* string, int resultado){
+  snprintf(string, TAM, "%d", resultado);
+}
